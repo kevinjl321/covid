67a64872d34616faea278983982cc72739e016b3
Kevin Lan <kevinlan321@gmail.com>
2020-12-31 16:46:25 -0800

    changed titles + started dark theme

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ const App = () => {
     <div className="app">
       <div className="app__left">
         <div className="app__header">
-          <h1>COVID Visualizer</h1>
+          <h1>COVID-19 Visualizer</h1>
           <FormControl className="app__dropdown">
             <Select
               variant="outlined"
